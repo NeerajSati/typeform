c9868985b2d1607361d6975abac4d3a405972f5b
Neeraj Sati <neeraj.sati123@gmail.com>
2023-04-01 18:28:08 +0530

    User name inputs styled and states handled

--- src/components/Pagecover.jsx
+++ src/components/Pagecover.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.svg'
 import Renderpage from './Renderpage'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { object, string, number, date, InferType } from 'yup';
+
 function Pagecover() {
+    const formInitialValues = {firstName: '', lastName: ''};
+    const validationSchema = object({
+      firstName: string().required(),
+      lastName: string().required()
+    })
+    const [formIndex, setFormIndex] = useState(0)
   return (
     <div>
         <div className='h-16 w-screen flex flex-row'>
@@ -11,7 +20,19 @@ function Pagecover() {
         </div>
         <div className='w-full flex items-center justify-center' style={{minHeight: 'calc(100vh - 4.5rem)'}}>
             <div className='text-white w-[80vw] min-h-[20rem] translate-y-[-2rem] sm:w-[75vw] lg:w-[800px] flex flex-col justify-center items-start font-[Gilroy400] text-[25px] font-s'>
-                <Renderpage idx={0}/>
+            <Formik
+               initialValues={formInitialValues}
+               validationSchema={validationSchema}
+               onSubmit={(values) => {
+                 console.log(values)
+               }}
+             >
+               {({ isSubmitting, values }) => (
+                 <Form className="w-full">
+                  <Renderpage idx={formIndex} setFormIndex={setFormIndex} values={values}/>
+                 </Form>
+               )}
+            </Formik>
             </div>
         </div>
     </div>
--- src/components/Renderpage.jsx
+++ src/components/Renderpage.jsx
@@ -2,22 +2,50 @@ import React from 'react'
 import { BsArrowRight } from 'react-icons/bs';
 import { BiCheck } from 'react-icons/bi';
 import { AiOutlineEnter } from 'react-icons/ai';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-function Renderpage({idx}){
+function Renderpage({idx, setFormIndex, values}){
     switch(idx){
         case 0: return(<>
+            <div className='flex pl-10 '>Up-skilling requires time commitment</div>
+            <div className='flex pl-10 pt-2 text-[20px] text-[#ffffffb3]'>The GrowthX experience is designed by keeping in mind the working hours founders & full time operators typically work in.</div>
+            <div className='flex pl-10 pt-10 text-[20px] text-[#ffffffb3]'>You will spend</div>
+            <div className='flex pl-10 text-[20px] text-[#ffffffb3]'>- 6 hours/week for the first 5 weeks</div>
+            <div className='flex pl-10 text-[20px] text-[#ffffffb3]'>- 15 hours/week for the last 3 weeks</div>
+            <div className='w-full pl-10 mt-5'>
+                <div className='flex items-center'>
+                    <button className='bg-[#0077ff] p-4 pt-2 pb-2 flex text-[20px] font-[Gilroy700] rounded-md mt-4' onClick={()=>{setFormIndex(i => i+1)}}>OK<BiCheck className='text-[1.8rem]'/></button>
+                    <span className='text-[13px] ml-3 mt-3'>press </span>
+                    <span className='text-[13px] font-[Gilroy700] pl-1 flex mt-3'>Enter <AiOutlineEnter className='mt-1 ml-1'/></span>
+                </div>
+            </div>
+            </>);
+        case 1: return(<>
             <div className='flex'><span className='text-[1rem] flex pt-[0.4rem]'>1 <BsArrowRight className='ml-1 mr-4 mt-[0.2rem]'/></span> What's your first name? *</div>
             <div className='w-full pl-10 mt-5'>
-                <input className='bg-black w-full pb-2 text-[30px] shadow-[0_1px_0px_0px_rgba(255,255,255,0.3)] focus:outline-none focus:shadow-[0_2px_0px_0px_rgba(255,255,255,1)]' style={{transition:'background-color 1e+08s ease 0s, box-shadow 0.1s ease-out 0s'}}></input>
+                <Field type="firstName" name="firstName" className='bg-black w-full pb-2 text-[30px] shadow-[0_1px_0px_0px_rgba(255,255,255,0.3)] focus:outline-none focus:shadow-[0_2px_0px_0px_rgba(255,255,255,1)]' style={{transition:'background-color 1e+08s ease 0s, box-shadow 0.1s ease-out 0s'}}></Field>
                 <div className='flex items-center'>
-                    <button className='bg-[#0077ff] p-4 pt-2 pb-2 flex text-[20px] font-[Gilroy700] rounded-md mt-4'>OK<BiCheck className='text-[1.8rem]'/></button>
+                    <button className='bg-[#0077ff] p-4 pt-2 pb-2 flex text-[20px] font-[Gilroy700] rounded-md mt-4' onClick={()=>{setFormIndex(i => i+1)}}>OK<BiCheck className='text-[1.8rem]'/></button>
+                    <span className='text-[13px] ml-3 mt-3'>press </span>
+                    <span className='text-[13px] font-[Gilroy700] pl-1 flex mt-3'>Enter <AiOutlineEnter className='mt-1 ml-1'/></span>
+                </div>
+            </div>
+            </>);
+        case 2: return(<>
+            <div className='flex'><span className='text-[1rem] flex pt-[0.4rem]'>2 <BsArrowRight className='ml-1 mr-4 mt-[0.2rem]'/></span> and your last name, {values.firstName}? *</div>
+            <div className='w-full pl-10 mt-5'>
+                <Field type="lastName" name="lastName" className='bg-black w-full pb-2 text-[30px] shadow-[0_1px_0px_0px_rgba(255,255,255,0.3)] focus:outline-none focus:shadow-[0_2px_0px_0px_rgba(255,255,255,1)]' style={{transition:'background-color 1e+08s ease 0s, box-shadow 0.1s ease-out 0s'}}></Field>
+                <div className='flex items-center'>
+                    <button className='bg-[#0077ff] p-4 pt-2 pb-2 flex text-[20px] font-[Gilroy700] rounded-md mt-4' onClick={()=>{setFormIndex(i => i+1)}}>OK<BiCheck className='text-[1.8rem]'/></button>
                     <span className='text-[13px] ml-3 mt-3'>press </span>
                     <span className='text-[13px] font-[Gilroy700] pl-1 flex mt-3'>Enter <AiOutlineEnter className='mt-1 ml-1'/></span>
                 </div>
             </div>
             </>);
         default: return(
-            <div>Temp</div>
+            <div>
+                Form ended.
+            </div>
             );
     }
 }
