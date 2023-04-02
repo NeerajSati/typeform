589606162b2e84f6a7204169d579b8259fa049ab
Neeraj Sati <neeraj.sati123@gmail.com>
2023-04-02 12:57:54 +0530

    Select tag created and designed

--- src/components/Renderpage.jsx
+++ src/components/Renderpage.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { BsArrowRight } from 'react-icons/bs';
 import { BiCheck } from 'react-icons/bi';
 import { AiOutlineEnter } from 'react-icons/ai';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Field } from 'formik';
+import {SelectTag} from './SelectTag';
+import IndustriesList from '../assets/industries.txt'
 
 function Renderpage({idx, setFormIndex, values}){
+    const [industriesOptions, setIndustriesOptions] = useState([]);
+    useEffect(() => {
+        fetch(IndustriesList)
+        .then(r => r.text())
+        .then(text => {
+            const temp = text.split('\r\n');
+            const arr = [];
+            for(let i=0;i<temp.length;i++){
+                arr.push({
+                    label: temp[i],
+                    value: temp[i]
+                  })
+            }
+            setIndustriesOptions(arr)
+       });
+    }, [])
+
     switch(idx){
         case 0: return(<>
             <div className='flex pl-10 '>Up-skilling requires time commitment</div>
@@ -35,6 +54,25 @@ function Renderpage({idx, setFormIndex, values}){
             <div className='flex'><span className='text-[1rem] flex pt-[0.4rem]'>2 <BsArrowRight className='ml-1 mr-4 mt-[0.2rem]'/></span> and your last name, {values.firstName}? *</div>
             <div className='w-full pl-10 mt-5'>
                 <Field type="lastName" name="lastName" className='bg-black w-full pb-2 text-[30px] shadow-[0_1px_0px_0px_rgba(255,255,255,0.3)] focus:outline-none focus:shadow-[0_2px_0px_0px_rgba(255,255,255,1)]' style={{transition:'background-color 1e+08s ease 0s, box-shadow 0.1s ease-out 0s'}}></Field>
+                <div className='flex items-center'>
+                    <button className='bg-[#0077ff] p-4 pt-2 pb-2 flex text-[20px] font-[Gilroy700] rounded-md mt-4' onClick={()=>{setFormIndex(i => i+1)}}>OK<BiCheck className='text-[1.8rem]'/></button>
+                    <span className='text-[13px] ml-3 mt-3'>press </span>
+                    <span className='text-[13px] font-[Gilroy700] pl-1 flex mt-3'>Enter <AiOutlineEnter className='mt-1 ml-1'/></span>
+                </div>
+            </div>
+            </>);
+        case 3: return(<>
+            <div className='flex'><span className='text-[1rem] flex pt-[0.4rem]'>3 <BsArrowRight className='ml-1 mr-4 mt-[0.2rem]'/></span> What industry is your company in? *</div>
+            <div className='flex pl-10 pt-2 text-[20px] text-[#ffffffb3]'>We will personalize your learning experience accordingly</div>
+            <div className='w-full pl-10 mt-5'>
+                <Field
+                  className="custom-select"
+                  name="singleLanguage"
+                  options={industriesOptions}
+                  component={SelectTag}
+                  placeholder="Select a language..."
+                  isMulti={false}
+                />
                 <div className='flex items-center'>
                     <button className='bg-[#0077ff] p-4 pt-2 pb-2 flex text-[20px] font-[Gilroy700] rounded-md mt-4' onClick={()=>{setFormIndex(i => i+1)}}>OK<BiCheck className='text-[1.8rem]'/></button>
                     <span className='text-[13px] ml-3 mt-3'>press </span>
---
+++ src/components/SelectTag.jsx
@@ -0,0 +1,106 @@
+import { FieldProps } from "formik";
+import React from "react";
+import Select from "react-select";
+
+export const SelectTag = ({
+  className,
+  placeholder,
+  field,
+  form,
+  options,
+  isMulti = false
+}) => {
+
+    const colourStyles = {
+        menu: (provided, state) => ({
+            ...provided,
+            backgroundColor: 'black',
+            outline: 'none',
+            paddingLeft: '0px',
+            border: 'none'
+          }),
+
+        placeholder: (defaultStyles) => {
+            return {
+                ...defaultStyles,
+                color: '#ffffff'
+            }
+        },
+        
+        control: (styles, { data, isDisabled, isFocused, isSelected }) => ({ ...styles, 
+            backgroundColor: 'black', 
+            outline: 'none', 
+            borderRadius: '0px',
+            color: 'white',
+            border: 'none',
+            boxShadow: isFocused ? 'rgba(255, 255, 255) 0px 2px' : 'rgba(255, 255, 255, 0.3) 0px 1px',
+            "&:hover": {
+                boxShadow: 'rgba(255, 255, 255) 0px 2px',
+                cursor: 'text'
+            }
+        }),
+        option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+          return {
+            ...styles,
+            backgroundColor: 'rgb(26, 26, 26)',
+            color: '#FFF',
+            borderRadius: '5px',
+            border: 'solid 1px white',
+            marginBottom: '5px',
+            cursor: isDisabled ? 'not-allowed' : 'default',
+            fontSize: '20px',
+            padding: '5px',
+            paddingLeft: '10px',
+            "&:hover": {
+                backgroundColor: "rgb(76 76 76)",
+                cursor: 'pointer'
+            }
+          }
+        },
+      };
+  const onChange = (option) => {
+    form.setFieldValue(
+      field.name,
+      isMulti
+        ? (option).map((item) => item.value)
+        : (option).value
+    );
+  };
+
+  const getValue = () => {
+    if (options) {
+      return isMulti
+        ? options.filter(option => field.value.indexOf(option.value) >= 0)
+        : options.find(option => option.value === field.value);
+    } else {
+      return isMulti ? [] : "";
+    }
+  };
+
+  return (
+    <Select
+      className={className}
+      name={field.name}
+      value={getValue()}
+      onChange={onChange}
+      placeholder={placeholder}
+      options={options}
+      isMulti={isMulti}
+      styles={colourStyles}
+      components={{
+        IndicatorSeparator: () => null
+      }}
+      theme={(theme) => ({
+        ...theme,
+        borderRadius: 0,
+        colors: {
+        ...theme.colors,
+          neutral80: 'white',
+          neutral40: 'white'
+        },
+      })}
+    />
+  );
+};
+
+export default SelectTag;
