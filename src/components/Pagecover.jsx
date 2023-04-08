914d4b061b5e873922acc9c90794fc45e8df0470
Neeraj Sati <neeraj.sati123@gmail.com>
2023-04-08 21:15:25 +0530

    Form data sent in POST API

--- src/components/Pagecover.jsx
+++ src/components/Pagecover.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from 'react'
 import logo from '../assets/logo.svg'
 import Renderpage from './Renderpage'
+import axios from 'axios'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { object, string, number, date, InferType } from 'yup';
 
 function Pagecover() {
-    const formInitialValues = {firstName: '', lastName: ''};
+    const formInitialValues = {firstName: '', lastName: '', industry: '', email: '', country: 'India', phone: ''};
     const validationSchema = object({
-      firstName: string().required(),
-      lastName: string().required()
+      firstName: string().required('Your First Name is required!'),
+      lastName: string().required('Your Last Name is required!'),
+      industry: string().required('Select at least one option!'),
+      email: string().email('Invalid Email!').required('Your Email is required!'),
+      country: string(),
+      phone: number().required('Your Phone Number is required!'),
     })
     const [formIndex, setFormIndex] = useState(0)
+    const submitForm = (values) => {
+      console.log(values)
+      axios.post('https://eo3oi83n1j77wgp.m.pipedream.net', values)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+
   return (
     <div>
         <div className='h-16 w-screen flex flex-row'>
@@ -23,13 +39,11 @@ function Pagecover() {
             <Formik
                initialValues={formInitialValues}
                validationSchema={validationSchema}
-               onSubmit={(values) => {
-                 console.log(values)
-               }}
+               onSubmit={submitForm}
              >
                {({ isSubmitting, values }) => (
                  <Form className="w-full">
-                  <Renderpage idx={formIndex} setFormIndex={setFormIndex} values={values}/>
+                  <Renderpage idx={formIndex} setFormIndex={setFormIndex} values={values} isSubmitting={isSubmitting}/>
                  </Form>
                )}
             </Formik>
